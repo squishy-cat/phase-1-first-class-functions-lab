cd0129c2e3941848090ed85ab21bfa190296b25d
Eve Kozina <eve.kozina@gmail.com>
2021-11-01 23:28:21 -0400

    commit tree 9a91c7b4b6f12b3ccbd4789e01f349557f97556f

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+// const returnFirstTwoDrivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+// (function () {
+//     return returnFirstTwoDrivers.slice(0,2)
+// });
+
+
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+function returnFirstTwoDrivers () {
+    return drivers.slice(0,2);
+}
+
+function returnLastTwoDrivers() {
+    return drivers.slice(drivers.length - 2, drivers.length);
+}
+
+let selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+]
+
+function createFareMultiplier(multiplier) {
+    return function (fare) {
+        return fare * multiplier
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(driversArray, driversSelectFunction) {
+    return driversSelectFunction(driversArray);
+}
